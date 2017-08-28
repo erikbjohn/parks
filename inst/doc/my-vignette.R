a6b5d1553ec818a60447928d57b166155e02db65
erikbjohn <erikbjohn@gmail.com>
2017-08-28 14:11:25 -0500

    Updated vignette to include example output table

--- inst/doc/my-vignette.R
+++ inst/doc/my-vignette.R
@@ -1,18 +1,7 @@
 ## ---- eval=TRUE, size='tiny', echo=FALSE, warning=FALSE------------------
-library(data.table)
 library(knitr)
-dt <- data.table(address = '6975 E Sandown Rd, Denver, CO 80216',
-                 copyright = '© 2017 Google',
-                 date = '2016-09',
-                 location.lat = '39.7...',
-                 location.lng = '-104.9...',
-                 pano_id = 'LDQK5dSu5...',
-                 statius = 'OK'
-                 )
-kable(dt, table.attr = "style='width:100%;'")
-
-## ---- eval=TRUE, cache=TRUE, fig.width=6, fig.height=6-------------------
-library(streetview)
-l.plots <- streetview::plot_panoids(path.root = '~/Dropbox/pkg.data')
-l.plots$year
+parks.shapes <- get_parks()
+plot(parks.shapes)
+table(parks.shapes@data$park_type)
+knitr::kable(parks.shapes@data[1:20,])
 
